47b284055e126e24114b652a29959c35f372ead9
trian1010 <s3974923@rmit.edu.vn>
2024-04-06 13:37:38 +0700

    Create DataInput.java
    Create DataInput class to assist admin to input essential information.

---
+++ DataInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DataInput {
+    private static DataInput input;
+    private Scanner sc;
+    private DataInput() {
+        sc = new Scanner(System.in);
+    }
+
+    public static DataInput getDataInput() {
+        if (input == null) {
+            input = new DataInput();
+        }
+        return input;
+    }
+
+    public Scanner getScanner() {
+        return sc;
+    }
+}
