0fa1ec200d11dc4b9312b5c351a9db3b145fa3be
trian1010 <s3974923@rmit.edu.vn>
2024-04-06 13:46:47 +0700

    Create ClaimView and ClaimController
    Create ClaimView to create essential attributes and override methods, Create ClaimController to implement methods to execute functions in the system.

---
+++ ClaimController.java
@@ -0,0 +1,119 @@
+import java.util.*;
+
+public class ClaimController {
+    // Attributes
+    HashMap<String, Claim> claimList;
+    Claim claim;
+    ClaimView view;
+
+    // Constructor
+    public ClaimController(Claim claim, ClaimView view) {
+        this.claim = claim;
+        this.view = view;
+        this.claimList = new HashMap<>();
+    }
+
+    // Method add
+    public void add() {
+        String answer = "Yes";
+        Scanner scanner = DataInput.getDataInput().getScanner();
+        while (answer.equalsIgnoreCase("Yes")) {
+            HashMap<String, String> data = view.displayNewClaimForm();
+            String id = data.get(ClaimView.CLAIM_ID);
+            Date claimDate = new Date(Long.parseLong(data.get("CLAIM_DATE"))); // Convert String to Date
+            String insuredPerson = data.get(ClaimView.INSURED_PERSON);
+            String cardNumber = data.get(ClaimView.CARD_NUMBER);
+            Date examDate = new Date(Long.parseLong(data.get("EXAM_DATE"))); // Convert String to Date
+            ArrayList<String> documentList = new ArrayList<>(Arrays.asList(data.get(String.valueOf(ClaimView.DOCUMENT_LIST)).split(","))); // Convert String to ArrayList
+            double claimAmount = Double.parseDouble(data.get(String.valueOf(ClaimView.CLAIM_AMOUNT))); // Convert String to double
+            String status = data.get(ClaimView.STATUS);
+            ReceiverBankingInfo receiverBankingInfo = new ReceiverBankingInfo(data.get(ClaimView.RECEIVER_BANKING_INFO_BANK), data.get(ClaimView.RECEIVER_BANKING_INFO_NAME), data.get(ClaimView.RECEIVER_BANKING_INFO_NUMBER));
+
+
+            claim = new Claim(id, claimDate, insuredPerson, cardNumber, examDate, documentList, claimAmount, status, receiverBankingInfo);
+
+            claimList.put(id, claim); //Add the claim to the claimList
+
+            view.display(claim);
+
+            System.out.println("Continue? (Yes/No) ");
+            answer = scanner.nextLine();
+        }
+    }
+
+    // Method get all
+    public void getAll() {
+        if (claimList.isEmpty()) {
+            System.out.println("No claims available.");
+        } else {
+            System.out.println("All Claims:");
+            System.out.println();
+            Iterator<Claim> iterator = claimList.values().iterator(); // Use iterator to get all Claim in the HashMap
+            while (iterator.hasNext()) {
+                Claim claim = iterator.next();
+                view.display(claim);
+            }
+        }
+    }
+
+    // Method get one
+    public void getOne() {
+        Scanner scanner = DataInput.getDataInput().getScanner();
+        HashMap<String, String> data = view.displayGetOneClaimForm();
+        String id = data.get(ClaimView.CLAIM_ID);
+
+        if (claimList.containsKey(id)) {
+            Claim claim = claimList.get(id);
+            view.display(claim);
+        } else {
+            System.out.println("Claim with the ID " + id + " not found.");
+        }
+    }
+
+    // Method delete
+    public void delete() {
+        Scanner scanner = DataInput.getDataInput().getScanner();
+        HashMap<String, String> data = view.displayDeleteClaimForm();
+        String id = data.get(ClaimView.CLAIM_ID);
+
+        if(claimList.containsKey(id)) {
+            Claim claim = claimList.get(id);
+            System.out.println("Claim with the ID " + id + " has been removed.");
+            claimList.remove(id);
+        } else {
+            System.out.println("Claim with the ID " + id + " not found.");
+        }
+    }
+
+    // Method update
+    public void update() {
+        Scanner scanner = DataInput.getDataInput().getScanner();
+        HashMap<String, String> data = view.displayUpdateClaimForm();
+        String id = data.get(ClaimView.CLAIM_ID);
+
+        if(claimList.containsKey(id)) {
+            Claim claim = claimList.get(id);
+            String insuredPerson = data.get(ClaimView.INSURED_PERSON);
+            String cardNumber = data.get(ClaimView.CARD_NUMBER);
+            Date examDate = new Date(Long.parseLong(data.get("EXAM_DATE"))); // Convert String to Date
+            ArrayList<String> documentList = new ArrayList<>(Arrays.asList(data.get(String.valueOf(ClaimView.DOCUMENT_LIST)).split(","))); // Convert String to ArrayList
+            double claimAmount = Double.parseDouble(data.get(String.valueOf(ClaimView.CLAIM_AMOUNT))); // Convert String to double
+            String status = data.get(ClaimView.STATUS);
+            ReceiverBankingInfo receiverBankingInfo = new ReceiverBankingInfo(data.get(ClaimView.RECEIVER_BANKING_INFO_BANK), data.get(ClaimView.RECEIVER_BANKING_INFO_NAME), data.get(ClaimView.RECEIVER_BANKING_INFO_NUMBER));
+
+            // Update specific attributes (ClaimID and ClaimDate will not be update since ClaimDate is the date that the Claim is created and ClaimID must remain the same)
+            claim.setInsuredPerson(insuredPerson);
+            claim.setCardNumber(cardNumber);
+            claim.setExamDate(new Date());
+            claim.setDocumentList(documentList);
+            claim.setClaimAmount(claimAmount);
+            claim.setStatus(status);
+            claim.setReceiverBankingInfo(receiverBankingInfo);
+
+            // Update claim in the claimList
+            claimList.put(id, claim);
+
+            view.display(claim);
+        }
+    }
+}
---
+++ ClaimView.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+public abstract class ClaimView {
+    public static final String CLAIM_ID = "CLAIM_ID";
+    public static final Date CLAIM_DATE = new Date();
+    public static final String INSURED_PERSON = "INSURED_PERSON";
+    public static final String CARD_NUMBER = "CARD_NUMBER";
+    public static final Date EXAM_DATE = new Date();
+    public static final ArrayList<String> DOCUMENT_LIST = new ArrayList<String>();
+    public static final double CLAIM_AMOUNT = 0.0;
+    public static final String STATUS = "STATUS";
+    public static final String RECEIVER_BANKING_INFO_BANK = "RECEIVER_BANKING_INFO_BANK";
+    public static final String RECEIVER_BANKING_INFO_NAME = "RECEIVER_BANKING_INFO_NAME";
+    public static final String RECEIVER_BANKING_INFO_NUMBER = "RECEIVER_BANKING_INFO_NUMBER";
+
+
+
+    public abstract void displayMainMenu(ClaimController controller);
+
+    public abstract void display(Claim c);
+
+    public abstract HashMap<String, String> displayNewClaimForm();
+
+    public abstract HashMap<String, String> displayGetOneClaimForm();
+
+    public abstract HashMap<String, String> displayDeleteClaimForm();
+
+    public abstract HashMap<String, String> displayUpdateClaimForm();
+}
